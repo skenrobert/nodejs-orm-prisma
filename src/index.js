1f47a678569ec37d893d4d3c9068c5b051da0e0a
kenny_robert <skenrobert@gmail.com>
2023-07-09 13:08:04 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import productRoutes from "./routes/products.routes.js";
 import categoryRoutes from "./routes/categories.routes.js";
 import userRoutes from "./routes/users.routes.js";
-import jwt from "jsonwebtoken";
+
+
+// import jwt from "jsonwebtoken";
+// import { prisma } from "./db.js";
+// import bcrypt  from "bcrypt";
 
 const app = express();
 
@@ -12,66 +16,90 @@ app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", userRoutes);
 
-app.post("/api/login", async (req, res, next) => {
+// app.post("/api/login", async (req, res, next) => {
 
-    const email = req.body.email;
-    const password = req.body.user;
+//     const email = req.body.email? req.body.email: '' ;
+//     const password = req.body.password? req.body.password: '' ;
 
     
-    if(email != "" && password != ""){
-        try {
-                const userdb = await prisma.user.findOne({ email: email }); 
+//     if(email != "" && password != ""){
+//             try {
+//                     const userdb = await prisma.user.findUnique({ 
+//                         where: {
+//                             email: email,
+//                         },
+//                     });  
 
-             res.json(userdb);
+//                     if(userdb != null){
+//                         if(! bcrypt.compareSync(password ,userdb.password? userdb.password: '')){
+//                             // res.sendStatus(400);
+//                             res.sendStatus(400).json({
+//                                 ok: false,
+//                                 err: {
+//                                 message: "incorrect password"
+//                                 }
+//                             });
+//                         }else {
 
-        } catch (error) {
-            // console.log({error});
-            console.log(error);
-            console.log(error.stack);
-            next(error);
-        }
+//                             const user = {userdb};
+//                             // expiresIn: process.env.CADUCIDAD_TOKEN
+//                             const token = jwt.sign({user}, 'my_secret_key', {expiresIn: '10h'});  
+//                             res.json({
+//                                     user: userdb,
+//                                     msj : "is ready jwt",
+//                                     token
+//                                 });
+//                         }
+//                     }else{
+//                         res.sendStatus(400).json({
+//                             err: {
+//                                 message: "incorrect email"
+//                                 },
+//                             ok: false
+
+//                         });
+
+//                     }    
+                        
+//             } catch (error) {
+//                 // console.log({error});
+//                 // console.log(error);
+//                 // console.log(error.stack);
+//                 next(error);
+//             }
+
+//     }else{
+//         console.log('empty');
+//     }
+
+// });
 
 
-    }else{
-        console.log('empty');
-    }
+// app.get('/api/proctected', middletoken, (req, res)=> {
+//     jwt.verify(req.token, 'my_secret_key', (err, data) => {
+//         if(err){
+//             res.sendStatus(403);
+//         }else {
+//             res.json({
+//                 test: 'protected',
+//                 data
+//             });
+//         }
+//     });
+// });
 
-
-    const user = {id: 3}
-    const token = jwt.sign({user}, 'my_secret_key', {expiresIn: '10h'});  
-    res.json({
-            msj : "nodejs and jwt",
-            token
-        });
-});
-
-
-app.get('/api/proctected', middletoken, (req, res)=> {
-    jwt.verify(req.token, 'my_secret_key', (err, data) => {
-        if(err){
-            res.sendStatus(403);
-        }else {
-            res.json({
-                test: 'protected',
-                data
-            });
-        }
-    });
-});
-
-
-function middletoken(req, res, next){
-    const bearerHeader = req.headers['authorization'];
-    console.log(bearerHeader);
-    if(typeof bearerHeader != 'undefined'){
-        const bearer = bearerHeader.split(" ");
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    }else{ 
-        res.sendStatus(403);
-    }
-}
+// function middletoken(req, res, next){
+//     const bearerHeader = req.headers['authorization'];
+//     console.log(bearerHeader);
+//     if(typeof bearerHeader != 'undefined'){
+//         const bearer = bearerHeader.split(" ");
+//         const bearerToken = bearer[1];
+//         req.token = bearerToken;
+//         next();
+//     }else{ 
+//         res.sendStatus(403);
+//     }
+// }
 
 
 app.listen(3000);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,50 +1,136 @@
 import { Router } from "express";
 import { prisma } from "../db.js";
-import bcrypt  from "bcrypt";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
 router.get("/users", async (req, res, next) => {
-	try {
-		// get all user findMany
-		const users = await prisma.user.findMany({
-			// include: {
-			// 	category: true,
-			// },
-		});
-		res.json(users);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    // get all user findMany
+    const users = await prisma.user.findMany({
+      // include: {
+      // 	category: true,
+      // },
+    });
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 });
-
 
 router.post("/users", async (req, res, next) => {
+  const salt = bcrypt.genSaltSync(10);
+  // const hash = bcrypt.hashSync(myPlaintextPassword, salt);
 
-    const salt = bcrypt.genSaltSync(10);
-    // const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+  try {
+    let body = req.body;
+    let { username, email, password } = body;
 
-	try {
+    let userObj = {
+      username,
+      email,
+      //password: bcrypt.hashSync(password, salt)
+      password: await bcrypt.hash(password, 10),
+    };
 
-        let body = req.body;
-        let { username, email, password } = body;
-
-        let userObj = {
-            username,
-            email,
-            //password: bcrypt.hashSync(password, salt)
-            password: await bcrypt.hash(password,10)
-          };
-
-		const user = await prisma.user.create({
-			data: userObj,
-		});
-		// console.log({user});
-		res.json(user);
-	} catch (error) {
-		console.log({error});
-		next(error);
-	}
+    const user = await prisma.user.create({
+      data: userObj,
+    });
+    // console.log({user});
+    res.json(user);
+  } catch (error) {
+    console.log({ error });
+    next(error);
+  }
 });
+
+// **************************************************************
+
+router.post("/login", async (req, res, next) => {
+  const email = req.body.email ? req.body.email : "";
+  const password = req.body.password ? req.body.password : "";
+
+  if (email != "" && password != "") {
+    try {
+      const userdb = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (userdb != null) {
+        if (
+          !bcrypt.compareSync(password, userdb.password ? userdb.password : "")
+        ) {
+          // res.sendStatus(400);
+          res.sendStatus(400).json({
+            ok: false,
+            err: {
+              message: "incorrect password",
+            },
+          });
+        } else {
+          const user = { userdb };
+          // expiresIn: process.env.CADUCIDAD_TOKEN
+          const token = jwt.sign({ user }, "my_secret_key", {
+            expiresIn: "10h",
+          });
+          res.json({
+            user: userdb,
+            msj: "is ready jwt",
+            token,
+          });
+        }
+      } else {
+        res.sendStatus(400).json({
+          err: {
+            message: "incorrect email",
+          },
+          ok: false,
+        });
+      }
+    } catch (error) {
+      // console.log({error});
+      // console.log(error);
+      // console.log(error.stack);
+      next(error);
+    }
+  } else {
+    console.log("empty");
+  }
+});
+
+router.get("/proctected", middletoken, (req, res) => {
+  jwt.verify(req.token, "my_secret_key", (err, data) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        test: "protected",
+        data,
+      });
+    }
+  });
+});
+
+router.get("/logout", (req, res) => {
+//   req.session.destroy(() => {
+//     res.redirect("/");
+//   });
+});
+
+function middletoken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  console.log(bearerHeader);
+  if (typeof bearerHeader != "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 export default router;
